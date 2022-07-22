fcbdf8502cd58a2b796af540f7e97274d3d2810a
Anmol Srivastava <anmol.srivastava1@flipkart.com>
2022-07-22 10:28:02 +0530

    init

---
+++ src/DemoJavaProject/DemoJavaProject.java
@@ -0,0 +1,9 @@
+package DemoJavaProject;
+
+import java.io.IOException;
+
+public class DemoJavaProject {
+    public static void main(String args[]) throws IOException {
+        System.out.println("Demo Java Project");
+    }
+}
